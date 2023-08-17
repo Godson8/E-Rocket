015efcd8890e8e4177383d711ff91ccdbd16770e
Godson Oladipupo <godsonoladipupo6@gmail.com>
2023-08-17 11:53:31 -0600

    Fixed index location

--- Frontend/app/index.tsx
+++ Frontend/app/index.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import Logo from "../../components/Logo";
+// import Logo from "../components/Logo";
 import "./globals.css";
-import Header from "../../components/Header";
-import Hero from "../../components/Hero";
-import FirstSection from "../../components/FirstSection";
-import SecondSection from "../../components/SecondSection";
-import Footer from "../../components/Footer";
-import MidSection from "../../components/MidSection";
+import Header from "../components/Header";
+import Hero from "../components/Hero";
+import FirstSection from "../components/FirstSection";
+import SecondSection from "../components/SecondSection";
+import Footer from "../components/Footer";
+import MidSection from "../components/MidSection";
 
 const index = () => {
   return (
